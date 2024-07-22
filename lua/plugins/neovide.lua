a8ec4ae5792fdb63491d672222d54a11bc940a0f
Chooks22 <49726759+Chooks22@users.noreply.github.com>
2024-07-23 05:22:40 +0800

    ✨ add neovide

---
+++ lua/plugins/neovide.lua
@@ -0,0 +1,21 @@
+if not vim.g.neovide then return {} end
+
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      opt = {
+        guifont = "JetBrainsMonoNL Nerd Font:h12",
+        linespace = -1,
+      },
+      g = {
+        neovide_scale_factor = 1.0,
+        neovide_padding_top = 0,
+        neovide_padding_bottom = 0,
+        neovide_padding_right = 0,
+        neovide_padding_left = 0,
+      },
+    },
+  },
+}
